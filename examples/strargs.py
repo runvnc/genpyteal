d6c63e52919e727b4f62aef6f1a61f39f414baee
Jason Livesay <runvnc@gmail.com>
2022-01-14 18:55:27 -0600

    tests passing

--- examples/strargs.py
+++ examples/strargs.py
@@ -4,6 +4,7 @@ def app():
   age = Btoi(Txn.application_args[1])
   if age > 65:
     Log("User " + name + " is at retirement age.")
+    return 1
   else:
     Log("User " + name + " is still young.")
-  return 1
+    return 0
