8d980d55ea843d175051a4625ff46ceb6bf4fd06
Jason Livesay <runvnc@gmail.com>
2022-09-06 22:58:22 -0500

    update

--- lib/old_libex.py
+++ lib/old_libex.py
@@ -8,23 +8,23 @@ def asset_bal(addr, asset_index):
    )
 
 @Subroutine(TealType.anytype)
-def ggeti(key) -> Expr:
+def ggeti(key: TealType.bytes) -> Expr:
     maybe = App.globalGetEx(Int(0), key)
     return Seq(maybe, If(maybe.hasValue(), maybe.value(), Int(0)))
     
 @Subroutine(TealType.anytype)
-def lgeti(key) -> Expr:
+def lgeti(key: TealType.bytes) -> Expr:
     """Returns the result of a local storage MaybeValue if it exists, else return a default value"""
     mv = App.localGetEx(Int(0), Int(0), key)
     return Seq(mv, If(mv.hasValue()).Then(mv.value()).Else(Int(0)))
 
 @Subroutine(TealType.bytes)
-def ggets(key) -> Expr:
+def ggets(key: TealType.bytes) -> Expr:
     maybe = App.globalGetEx(Int(0), key)
     return Seq(maybe, If(maybe.hasValue(), maybe.value(), Bytes("")))
     
 @Subroutine(TealType.bytes)
-def lgets(key) -> Expr:
+def lgets(key: TealType.bytes) -> Expr:
     """Returns the result of a local storage MaybeValue if it exists, else return a default value"""
     mv = App.localGetEx(Int(0), Int(0), key)
     return Seq(mv, If(mv.hasValue()).Then(mv.value()).Else(Bytes("")))
